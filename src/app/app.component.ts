39133fdc27b012001c304208b99cea139dae7c18
vthota09 <vijayalaxmi1thota@gmail.com>
2022-01-05 22:03:44 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { User } from './user';
+import { EnrollmentService } from './enrollment.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angularforms';
-}
+  errorMsg: any;
+  title(title: any) {
+    throw new Error('Method not implemented.');
+  }
+  topics = ['Angular','React','Vue'];
+  topicHasError = true;
+  submitted = false;
+  errormsg = '';
+
+  userModel = new User('bubby', 'bubby@test.com', 5676764666, 'default','morning', true);
+  
+  constructor(private _enrollmentService: EnrollmentService){}
+
+  validateTopic(value: string) {
+    if (value === 'default') {
+      this.topicHasError = true;
+    } else {
+      this.topicHasError = false;
+    }
+    }
+  onSubmit(){
+    this.submitted = true;
+    this._enrollmentService.enroll(this.userModel)
+    .subscribe(
+      data => console.log('Success', data),
+      error => this.errormsg = error.statusText
+    )
+  }
+
+  }
